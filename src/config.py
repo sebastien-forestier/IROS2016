64964422bab427561c43f0d187b94ccb20965c27
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-02-15 23:37:34 +0100

    more xp

--- src/config.py
+++ src/config.py
@@ -339,12 +339,12 @@ config_list = {"xp1":["F-RmB",
                       "F-RGB",
                       "M-RMB",
                       "M-P-AMB",
-#                       "M-PMIN-AMB",
-#                       "M-GR-AMB",
-#                       "F-RGB-LWR",
-#                       "M-RMB-LWR",
-#                       "M-P-AMB-LWR",
-#                       "M-GR-AMB-LWR",
+                    "M-PMIN-AMB",
+                    "M-GR-AMB",
+                    "F-RGB-LWR",
+                    "M-RMB-LWR",
+                    "M-P-AMB-LWR",
+                    "M-GR-AMB-LWR",
                       ]}
 
 config = Config(name="F-RmB", hierarchy_type=0, babbling_name="motor", iterations=iterations)
