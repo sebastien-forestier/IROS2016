630739cb998eb2b712011a296c14a79f0b22c565
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-02-17 12:43:17 +0100

    analysis

--- src/analysis_draw_all.py
+++ src/analysis_draw_all.py
@@ -94,7 +94,7 @@ def mean_std_dic(d, add_0=False):
     return mean, std
     
 xp_name = "xp1"
-d = "2016-02-17_11-42-11-TOOL2-iros-xp1"
+d = "2016-02-17_12-42-33-TOOL2-iros-xp1"
 
 if os.environ.has_key("AVAKAS") and os.environ["AVAKAS"]:
     pref = ""
