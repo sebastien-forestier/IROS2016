7de6e697f3c0b9702b80d96414f5559a4ec63003
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-02-27 12:34:32 +0100

    analysis

--- src/analysis_comp.py
+++ src/analysis_comp.py
@@ -161,8 +161,8 @@ def main(explo_config_name, trial):
             fig, ax = plt.subplots()
             fig.canvas.set_window_title('Competence')
             for s_space in testcases.keys():
-                print x, comp[s_space][explo_config_name][trial][regression_config_name], np.median(comp[s_space][explo_config_name][trial][regression_config_name])
-                ax.plot(x, np.median(comp[s_space][explo_config_name][trial][regression_config_name]), label=s_space)
+                print x, comp[s_space][explo_config_name][trial][regression_config_name], np.median(comp[s_space][explo_config_name][trial][regression_config_name], axis=1)
+                ax.plot(x, np.median(comp[s_space][explo_config_name][trial][regression_config_name], axis=1), label=s_space)
             handles, labels = ax.get_legend_handles_labels()
             ax.legend(handles, labels)
                  
