d5b039fd977c5f0382e4f3e32714b61cacd02844
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-02-29 12:29:00 +0100

    exploit

--- src/evaluation.py
+++ src/evaluation.py
@@ -28,7 +28,8 @@ class Evaluation(object):
             print self.ag.modules["mod4"].sensorimotor_model.model.imodel.fmodel.dataset.nn_y(s_g)
         except:
             pass
-        self.ag.eval_mode()
+        self.ag.modules["mod4"].sensorimotor_model.mode = "exploit"
+        self.ag.modules["mod7"].sensorimotor_model.mode = "exploit"
         m = self.ag.modules[mid].sensorimotor_model.inverse(s_g)
         #action = self.ag.produce_module(mid, babbling=False, s=s_g, allow_explore=False)
         #m = action.get_m_seq(len(self.ag.conf.m_dims))[0]
