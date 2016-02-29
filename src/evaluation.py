cbd746eb3a1811f4f7d05d6dcd17a24234c2958c
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-02-29 12:11:39 +0100

    exploit

--- src/evaluation.py
+++ src/evaluation.py
@@ -29,7 +29,10 @@ class Evaluation(object):
         else:
             mid = "mod7"
         #print mid
-        print self.ag.modules["mod4"].sensorimotor_model.model.imodel.fmodel.dataset.nn_y(s_g)
+        try:
+            print self.ag.modules["mod4"].sensorimotor_model.model.imodel.fmodel.dataset.nn_y(s_g)
+        except:
+            pass
         action = self.ag.produce_module(mid, babbling=False, s=s_g, allow_explore=False)
         m = action.get_m_seq(len(self.ag.conf.m_dims))[0]
         s_env = self.env.update(m, log=False)
