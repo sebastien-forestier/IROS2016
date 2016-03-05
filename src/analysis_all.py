b74bb3d2072db892f22ea13b2ef62a8976b2fa34
Sébastien Forestier <sebastien.forestier@inria.fr>
2016-03-05 11:48:49 +0100

    analysis compl

--- src/analysis_all.py
+++ src/analysis_all.py
@@ -15,7 +15,7 @@ from analysis_events import main as main_events
 #                     "M-P-AMB-LWR",
 #                       ]}
 
-d = "2016-03-03_18-16-23-TOOL2-iros_complementary-xp_bootstrap"
+d = "2016-03-03_18-22-11-TOOL2-iros_complementary-xp_bootstrap"
 xp_name = "xp_bootstrap"
 
 if os.environ.has_key("AVAKAS") and os.environ["AVAKAS"]:
